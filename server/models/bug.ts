4b99e2645d29f4039ca54ce7713c7456cb0b7a3f
manav809 <manavpatel6978@gmail.com>
2024-05-06 19:57:49 -0400

    Add User and Bug Model

---
+++ server/models/bug.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const bugSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  assignee: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  story_points: { type: Number, required: false },
+});
+
+bugSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Bug = mongoose.model("Bug", bugSchema);
+module.exports = Bug;
---
+++ server/models/user.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  role: { type: String, required: true },
+  stories: { type: mongoose.Schema.Types.ObjectId, ref: "Bug" },
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
