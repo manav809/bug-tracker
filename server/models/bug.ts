68ab246aab73b2fc1b7f10ecbed3bf2918dceba1
manav809 <manavpatel6978@gmail.com>
2024-05-06 20:36:30 -0400

    Add Eslint and Gitignore Templates

--- server/models/bug.ts
+++ server/models/bug.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
 const bugSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  assignee: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  story_points: { type: Number, required: false },
-});
+  assignee: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  story_points: { type: Number, required: false }
+})
 
-bugSchema.set("toJSON", {
+bugSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id;
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-const Bug = mongoose.model("Bug", bugSchema);
-module.exports = Bug;
+const Bug = mongoose.model('Bug', bugSchema)
+module.exports = Bug
