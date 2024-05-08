9fc5445d52707d7044288f5ed2b2f239dc7919b2
manav809 <manavpatel6978@gmail.com>
2024-05-08 07:35:20 -0400

    Add Password Hashing

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -1,15 +1,25 @@
 import User from "../models/user";
 import { Router } from "express";
+import bcrypt from 'bcrypt'
 
 const userRouter: Router = Router();
 
 userRouter.post("/", async (req, res) => {
-  const { name, email, role } = req.body;
+  const { name, email, role, password } = req.body;
+
+  if (password.length < 3) {
+    res.status(401).json({
+      error: "Invalid Authentication: Password Must be at least 3 Characters"
+    })
+  }
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
 
   const user = new User({
     name,
     email,
     role,
+    passwordHash
   });
 
   const savedUser = await user.save();
